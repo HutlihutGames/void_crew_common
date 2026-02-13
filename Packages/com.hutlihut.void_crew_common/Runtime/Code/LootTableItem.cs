8c639292e9aa764a454a1dd3de2edd69c0467e4a
Kristijonas Malisauskas <kristijonas.malisauskas@hutlihut.dk>
2026-02-13 16:33:46 +0100

    wip prototyping drop table additions

---
+++ Packages/com.hutlihut.void_crew_common/Runtime/Code/LootTableItem.cs
@@ -0,0 +1,48 @@
+using System;
+using UnityEngine;
+
+namespace VC.Common
+{
+    [Flags]
+    public enum SectorDifficulty
+    {
+        Normal = 1 << 0,
+        Hard = 1 << 1,
+        Insane = 1 << 2,
+        Boss = 1 << 3,
+        All = Normal | Hard | Insane | Boss,
+    }
+
+    public enum LootRarity
+    {
+        Common = 1 << 0,
+        Rare = 1 << 1,
+        Epic = 1 << 2,
+        Legendary = 1 << 3,
+        Special = 1 << 4
+    }
+
+    [Serializable]
+    public class SectorCompletionReward
+    {
+        public int Weight = 1;
+
+        // Chapter consists of multiple normal objectives and a single boss encounter across a single solar system, everything post first chapter uses chapter index 1
+        public int[] Chapters = new[] { 0 };
+        public SectorDifficulty EncounterDifficulty = SectorDifficulty.Normal;
+    }
+
+    [Serializable]
+    public class EnemyKillReward
+    {
+        public int Weight = 1;
+        public int Quantity = 1;
+        public LootRarity Rarity;
+    }
+
+    public class LootTableItem : MonoBehaviour
+    {
+        public SectorCompletionReward[] SectorCompletionReward;
+        public EnemyKillReward[] EnemyKillReward;
+    }
+}
