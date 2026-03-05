cec77b099b4fb30d4cd736cc084eed4def7f5bbb
Kristijonas Malisauskas <kristijonas.malisauskas@hutlihut.dk>
2026-03-05 12:06:30 +0100

    Helmet for testing

---
+++ Packages/com.hutlihut.void_crew_common/Runtime/Code/Cosmetics/HelmetCosmetic.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace VC.Common.Cosmetics
+{
+    [CreateAssetMenu(fileName = "HelmetCosmetic", menuName = "Void Crew/Cosmetics/Helmet")]
+    public class HelmetCosmetic : VoidCrewCosmetic
+    {
+        public SkinnedMeshRenderer HelmetMesh;
+    }
+}
