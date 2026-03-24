d73fad42ca51df638ac5c1a9339c832748feb3e8
Kristijonas Malisauskas <kristijonas.malisauskas@hutlihut.dk>
2026-03-24 17:32:06 +0100

    Audio emitter data added

---
+++ Packages/com.hutlihut.void_crew_common/Runtime/Code/Audio/AudioEmitter.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace VC.Common.Audio
+{
+    public class AudioEmitter : MonoBehaviour
+    {
+        public AudioEventType EventType = AudioEventType.BASIC_3D_LOOP;
+        public AudioClip Clip;
+    }
+}
---
+++ Packages/com.hutlihut.void_crew_common/Runtime/Code/Audio/AudioEventType.cs
@@ -0,0 +1,18 @@
+namespace VC.Common.Audio
+{
+    public enum AudioEventType
+    {
+        BASIC_2D_ONESHOT,
+        BASIC_3D_ONESHOT,
+        BASIC_2D_LOOP,
+        BASIC_3D_LOOP,
+        CARRYABLE_IMPACT,
+        CARRYABLE_LOOP_CLOSE,
+        CARRYABLE_LOOP_MID,
+        CARRYABLE_LOOP_FAR,
+        EMOTE,
+        MODULE_TURN_OFF,
+        MODULE_TURN_ON,
+        PLAYER_SALUTE_VOICE,
+    }
+}
