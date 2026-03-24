bed4cf12755d41515979538c7312b9baad3b5452
Tobias Købke Nørregaard <tobias.koebke.norregaard@hutlihut.dk>
2026-03-24 14:33:42 +0100

    Fixed explorer opening multiple times per exported bundle

--- Packages/com.hutlihut.void_crew_common/Editor/Code/ExportAssetTools.cs
+++ Packages/com.hutlihut.void_crew_common/Editor/Code/ExportAssetTools.cs
@@ -93,10 +93,14 @@ namespace VC.Common.Editor
             AssetBundleManifest manifest = BuildPipeline.BuildAssetBundles(buildInput);
             if (manifest != null)
             {
+                var projectRelativePath = "";;
                 foreach (var bundleName in manifest.GetAllAssetBundles())
                 {
-                    string projectRelativePath = buildInput.outputPath + "/" + bundleName;
+                    projectRelativePath = buildInput.outputPath + "/" + bundleName;
                     Debug.Log($"Exported AssetBundle: {projectRelativePath}");
+                }
+                if (!string.IsNullOrEmpty(projectRelativePath))
+                {
                     EditorUtility.RevealInFinder(projectRelativePath);
                 }
             }
